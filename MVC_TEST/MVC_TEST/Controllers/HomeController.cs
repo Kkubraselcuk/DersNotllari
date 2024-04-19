ac13016a30536ec2c8f05f8f10adfe65e5a54faf
kkubraselcuk <kkubra.selcuk@gmail.com>
2024-04-19 16:55:29 +0300

    mvc not ekleme

---
+++ MVC_TEST/MVC_TEST/Controllers/HomeController.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MVC_TEST.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_TEST.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public string Metin()
+        {
+            return "Kitap controller sınıfındaki Index metodu çalıştı";
+        }
+
+        public JsonResult JsonCikti()
+        {
+            return Json(new
+            {
+                Id = 1,
+                Seviye = "11. Sınıf",
+                KitapAdi = "WEB TABANLI UYGULAMA GELİŞTİRME"
+            });
+        }
+
+        public FileResult Resim()
+        {
+            return File("~/meblogo.png", "image/png");
+        }
+
+        //Kitap/Liste/1
+        public string Liste(int id)
+        {
+            return $"Parametreden gelen değer: {id}";
+        }
+
+
+        //Kitap/Liste/1/MEB 
+        [Route("Kitap/Liste/{id}/{ad}")]
+        public string Liste(int id, string ad)
+        {
+            return $"Parametreden gelen id={id}, ad={ad}";
+        }
+
+
+
+
+
+
+
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MVC_TEST/MVC_TEST/Controllers/KitapController.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_TEST.Models;
+using System.Collections.Generic;
+using System.Text.Json;
+
+namespace MVC_TEST.Controllers
+{
+    public class KitapController : Controller
+    {
+        //6.2.4.2. Action Metottan View'e Veri Aktarımı
+        public IActionResult Index()
+        {
+            
+            var kitap = new List<KitapModel>
+            {
+               new KitapModel{ KitapId =1, KitapAdi="kitap 1", SayfaSayisi=100},
+               new KitapModel{ KitapId =1, KitapAdi="kitap 2", SayfaSayisi=200},
+               new KitapModel{ KitapId =1, KitapAdi="kitap 3", SayfaSayisi=300},
+            };
+            #region Model bazlı veri gönderilmesi
+            //return View(kitap);
+            #endregion;
+
+            #region Veri Taşıma Kontrolleri
+
+            #region ViewBag
+            //ViewBag.Baslik = "ViewBag ile veri gönderilmesi";
+            //ViewBag.Kitap = kitap;
+            #endregion
+
+            #region ViewData
+            ViewData["Baslik"] = "ViewData ile veri gönderilmesi";
+            ViewData["Kitap"] = kitap;
+            #endregion
+
+            #region TempData
+            TempData["Baslik"] = "TempData ile veri gönderilmesi";
+            TempData["Kitap"] = kitap;
+            #endregion
+
+            #endregion;
+            return View();
+        }
+        public IActionResult Index2()
+        {
+            #region Tempdata başka bir actiona veri gönderimi örneği
+            ViewBag.x = 5;
+            ViewData["x"] = 5;
+            TempData["x"] = 5;
+            #endregion
+            //RedirectToAction(Action, Controller);
+            return RedirectToAction("Index3");
+        }
+        public IActionResult Index3()
+        {
+            var x1 = ViewBag.x;
+            var x2 = ViewData["x"];
+            var x3 = TempData["x"];
+            return View();
+        }
+
+        // karmaşık yapıdaki verileri tempdata ile göndermek için
+        public IActionResult Index4()
+        {
+            var kitap = new List<KitapModel>
+            {
+               new KitapModel{ KitapId =1, KitapAdi="kitap 1", SayfaSayisi=100},
+               new KitapModel{ KitapId =1, KitapAdi="kitap 2", SayfaSayisi=200},
+               new KitapModel{ KitapId =1, KitapAdi="kitap 3", SayfaSayisi=300},
+            };
+            string data = JsonSerializer.Serialize(kitap);
+            TempData["kitaplar"] = data;
+            return View();
+        }
+        public IActionResult Index5()
+        {
+            var data = TempData["kitaplar"].ToString();
+            List<KitapModel> kitaplar = JsonSerializer.Deserialize<List<KitapModel>>(data);
+            return View();
+        }
+        public IActionResult Kategoriler()
+        {
+            return View();
+        }
+        public IActionResult KitapListesi(int id)
+        {
+            return View();
+        }
+
+
+    }
+}
